47f5c86d2c37eaf76cfa4b6e2641ec67f2f0fb1c
famkepj <82056762+famkepj@users.noreply.github.com>
2022-05-04 15:12:48 +0200

    use find

--- strings/main.py
+++ strings/main.py
@@ -10,16 +10,16 @@ player_two = 'Marco van Basten'
 goal_0 = 32
 goal_1 = 54
 
-scorers = (f'{player_one} {str(goal_0)}, {player_two} {str(goal_1)}')
+scorers = (player_one + ' ' + str(goal_0)+', '+ player_two + ' '+ str(goal_1))
 print (scorers)
 
 report = (f'{player_one} scored in the {goal_0}nd minute\n{player_two} scored in the {goal_1}th minute')
 print(report)
 
 player = 'Frank Rijkaard'
-first_name = (player[:5])
-last_name_len = (len(player[6:]))
-name_short = (player[:1]) + '. ' + (player[6:])
-chant = (first_name +'!'+ ' ') * (len(player[:5-1])) + (first_name +'!')
-good_chant = (chant[:-1] !=' ')
+first_name = (player[:player.find(' ')])
+last_name_len = (len(player[player.find(' ')+1:]))
+name_short = (player[:1]) + '. ' + (player[player.find(' ')+1:])
+chant = (first_name +'!'+ ' ') * (len(player[:player.find(' ')-1])) + (first_name +'!')
+good_chant = (chant[-1] !=' ')
 print(good_chant)
